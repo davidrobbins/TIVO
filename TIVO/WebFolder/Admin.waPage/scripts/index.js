3b31851ef61680f5baf619d860f6c4f1d12c59d4
David Robbins <dave@wakanda.org>
2012-10-13 13:42:41 -0700

    added admin user to directory

--- TIVO/WebFolder/Admin.waPage/scripts/index.js
+++ TIVO/WebFolder/Admin.waPage/scripts/index.js
@@ -11,6 +11,30 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	signInButton.click = function signInButton_click (event)// @startlock
 	{// @endlock
 		//SignIn button
+		if (WAF.directory.loginByPassword(WAF.sources.loginObject.loginName, WAF.sources.loginObject.loginPassword)) {
+			if (WAF.directory.currentUserBelongsTo("Admin")) {
+				/*
+				//hide login stuff
+				$$("richText1").setValue("Signed in as : " + WAF.directory.currentUser().fullName);
+				$$("signInContainer").hide();
+				$$("signOutContainer").show();
+				*/
+				
+				$("#mainContainer").show();
+				$("#splashContainer").css("top", "-1px");
+				$$("splashContainer").hide();
+				$$("loginTextField").setValue("");
+				$$("passwordTextField").setValue("");
+				//$$("signInError").setValue("");
+				
+			} else {
+				WAF.directory.logout();
+				$$("signInError").setValue("Only the Administrator can sign in to this application.");
+			} //(WAF.directory.currentUserBelongsTo("Administrator"))
+		
+	} else {
+		//$$("signInError").setValue("Invalid login.");
+	}
 	};// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
