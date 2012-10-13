ba7731d521acf825c635d849044ea7d565480014
David Robbins <dave@wakanda.org>
2012-10-13 13:27:30 -0700

    admin splashscreen. starting login container.

--- TIVO/WebFolder/Admin.waPage/scripts/index.js
+++ TIVO/WebFolder/Admin.waPage/scripts/index.js
@@ -2,17 +2,33 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var signInButton = {};	// @button
 	var documentEvent = {};	// @document
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
+	signInButton.click = function signInButton_click (event)// @startlock
+	{// @endlock
+		//SignIn button
+	};// @lock
+
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
-		
+		if (WAF.directory.currentUser() === null) {
+			$("#mainContainer").hide();
+			$("#splashContainer").css("top", "84px");
+			$("#splashContainer").css("left", "20px");
+			$("#splashContainer").show();
+		} else {
+			$("#mainContainer").show();
+			$("#splashContainer").css("top", "-1px");
+			$("#splashContainer").hide();
+		}
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("signInButton", "click", signInButton.click, "WAF");
 	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 // @endregion
 };// @endlock
