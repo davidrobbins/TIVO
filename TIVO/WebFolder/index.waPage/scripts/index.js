091a11f559a42989f4b7c4e682d671554ea83274
David Robbins <dave@wakanda.org>
2012-10-15 15:46:38 -0700

    added login to index page

--- TIVO/WebFolder/index.waPage/scripts/index.js
+++ TIVO/WebFolder/index.waPage/scripts/index.js
@@ -2,17 +2,40 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var signInButton = {};	// @button
+	var signoutButton = {};	// @button
 	var documentEvent = {};	// @document
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
+	signInButton.click = function signInButton_click (event)// @startlock
+	{// @endlock
+		// Sign In
+		tivoUtil.signIn();
+	};// @lock
+
+	signoutButton.click = function signoutButton_click (event)// @startlock
+	{// @endlock
+		//Sign Out
+		if (WAF.directory.logout()) {
+			//hide logout stuff
+			$$("signInRichText").setValue("");
+			$$("signOutContainer").hide();
+			$$("signInContainer").show();
+			$$("mainContainer").hide();
+			$("#splashContainer").css("top", "84px");
+			$("#splashContainer").css("left", "0px");
+			$$("splashContainer").show();	
+		}
+	};// @lock
+
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
 		// Add your code here
 		if (WAF.directory.currentUser() === null) {
-			//$$("signOutContainer").hide();
-			//$$("signInContainer").show();
+			$$("signOutContainer").hide();
+			$$("signInContainer").show();
 			
 			$("#mainContainer").hide();
 			$("#splashContainer").css("top", "84px");
@@ -20,8 +43,8 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 			$("#splashContainer").show();
 			
 		} else {
-			//$$("signOutContainer").show();
-			//$$("signInContainer").hide();
+			$$("signOutContainer").show();
+			$$("signInContainer").hide();
 			
 			$("#mainContainer").show();
 			$("#splashContainer").css("top", "-1px");
@@ -30,6 +53,8 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("signInButton", "click", signInButton.click, "WAF");
+	WAF.addListener("signoutButton", "click", signoutButton.click, "WAF");
 	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 // @endregion
 };// @endlock
