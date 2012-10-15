e7cfae6da4941fc7fc6b87622bc8db6fe62f03f4
David Robbins <dave@wakanda.org>
2012-10-15 15:10:21 -0700

    starting index page

---
+++ TIVO/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,35 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		// Add your code here
+		if (WAF.directory.currentUser() === null) {
+			//$$("signOutContainer").hide();
+			//$$("signInContainer").show();
+			
+			$("#mainContainer").hide();
+			$("#splashContainer").css("top", "84px");
+			$("#splashContainer").css("left", "0px");
+			$("#splashContainer").show();
+			
+		} else {
+			//$$("signOutContainer").show();
+			//$$("signInContainer").hide();
+			
+			$("#mainContainer").show();
+			$("#splashContainer").css("top", "-1px");
+			$("#splashContainer").hide();
+		}
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
